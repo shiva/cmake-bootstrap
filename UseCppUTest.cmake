3a3dd0b5a5c5cb873c3e4b339e04dcbd43270d6b
Shiva <shiv@shiv.me>
2016-06-19 21:04:39 -0700

    Find CppUTest libraries

--- UseCppUTest.cmake
+++ UseCppUTest.cmake
@@ -1,21 +1,18 @@
 include(CMakeParseArguments)
-include(CodeCoverage)
+#include(CodeCoverage)
 include(CTest)
 
 function(enable_unit_testing)
-    if(DEFINED ENV{CPPUTEST_HOME})
-        message("Using CppUTest found in $ENV{CPPUTEST_HOME}")
+    if(ENABLE_TESTING)
+        find_package(CppUTest)
+        message("Using CppUTest found in ${CPPUTEST_INCLUDE_DIR}")
+
+        include_directories(${CPPUTEST_INCLUDE_DIR})
+        include_directories(${CPPUTEST_EXT_INCLUDE_DIR})
+
         set(CMAKE_CXX_FLAGS "-g -O0 -fprofile-arcs -ftest-coverage")
         set(CMAKE_C_FLAGS "-g -O0 -fprofile-arcs -ftest-coverage")
         set(CMAKE_EXE_LINKER_FLAGS="-fprofile-arcs -ftest-coverage")
-
-        include_directories($ENV{CPPUTEST_HOME}/include)
-        add_library(imp_cpputest STATIC IMPORTED GLOBAL)
-        set_property(TARGET imp_cpputest PROPERTY
-            IMPORTED_LOCATION $ENV{CPPUTEST_HOME}/lib/libCppUTest.a)
-
-    else()
-        message("Disabling unit-tests. CPPUTEST_HOME is not set; You must tell CMake where to find CppUTest if you want to enable unit-testing.")
     endif()
 endfunction(enable_unit_testing)
 
@@ -23,27 +20,35 @@ function(add_unit_test)
     cmake_parse_arguments(ARGS "" "TARGET" "SOURCES" ${ARGN})
     set(ut_target "test-${ARGS_TARGET}")
 
+    if(NOT ENABLE_TESTING)
+        # skip if testing is not enabled
+        message(STATUS "UT disabled. Skipping test target: ${ut_target} ... ")
+        return()
+    endif()
+
     add_executable(
         ${ut_target}
         EXCLUDE_FROM_ALL
         ${ARGS_SOURCES}
     )
-    target_link_libraries(${ut_target} imp_cpputest)
+    target_link_libraries(${ut_target} ${CPPUTEST_LIBRARY})
 
     add_test(
         NAME ${ut_target}
         COMMAND $<TARGET_FILE:${ut_target}>
     )
 
+    # make check depends on all test targets
     if (NOT TARGET check)
         add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND})
     endif()
     add_dependencies(check ${ut_target})
 
-    SETUP_TARGET_FOR_COVERAGE(cov-${ut_target} ${ut_target} coverage-repot-${ut_target} "-v")
-    if (NOT TARGET coverage)
-        add_custom_target(coverage)
-    endif()
-    add_dependencies(coverage cov-${ut_target})
+    # comment on coverage for now
+    #SETUP_TARGET_FOR_COVERAGE(cov-${ut_target} ${ut_target} coverage-repot-${ut_target} "-v")
+    #if (NOT TARGET coverage)
+    #    add_custom_target(coverage)
+    #endif()
+    #add_dependencies(coverage cov-${ut_target})
 
 endfunction(add_unit_test)
