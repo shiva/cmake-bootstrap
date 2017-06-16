76b48d92fe5673132e975705f3e8847777cfc405
Shiva <shiv@shiv.me>
2017-06-15 23:14:36 -0700

    Support simpler coverage target

--- UseCppUTest.cmake
+++ UseCppUTest.cmake
@@ -19,23 +19,6 @@ function(add_unit_test)
         return()
     endif()
 
-#    if(WITH_COVERAGE)
-#        SETUP_TARGET_FOR_COVERAGE(
-#            cov-${ut_target}
-#            ${ut_target} coverage-report-${ut_target} "-v")
-#
-#        if (NOT TARGET coverage)
-#            add_custom_target(coverage
-#                COMMAND ${CMAKE_COMMAND} -DPROJECT_BINARY_DIR="${PROJECT_BINARY_DIR}" -P "${_CMAKE_SCRIPT_PATH}/CoverallsClear.cmake"
-#                WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-#            )
-#        endif()
-#        add_dependencies(coverage cov-${ut_target})
-#    else()
-#        message(STATUS "COVERAGE disabled. ")
-#    endif()
-
-
     # include paths and setup test target
     include_directories(${CPPUTEST_INCLUDE_DIRS})
     include_directories(${CPPUTEST_EXT_INCLUDE_DIRS})
